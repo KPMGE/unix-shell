9eb075836ca70aa3ff97d7e9d802ae36c0d73d49
KPMGE <jesuskevin254@gmail.com>
2023-06-17 00:14:01 -0300

    :sparkles: execute commands in foreground and background

--- src/helpers.c
+++ src/helpers.c
@@ -1,41 +1,36 @@
-#include <unistd.h>
+#include "../include/helpers.h"
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h>
-#include <stdbool.h>
-#include "../include/helpers.h"
-#include "helpers.h"
+#include <unistd.h>
 
 // Prototypes========//
 // Public
 char ***init_buffer();
-void r_strip(char * string);
-void end_buffer(char *** buffer);
-void end_commands(char ** commands);
+void r_strip(char *string);
+void end_buffer(char ***buffer);
+void end_commands(char **commands);
 void exec_commands_on_new_session(char ***commands, size_t amount_commads);
-char ***read_shell_input(char ***buffer, bool *foreground_exec, int * commands_amount);
+char ***read_shell_input(char ***buffer, bool *foreground_exec, int *commands_amount);
 
 // Private=========================================================//
-char ** split_commands(char * line, int * counter, char ** commands);
+static char **split_commands(char *line, int *counter, char **commands);
+static int split_args(char **commands, char ***buffer);
 
 // ======================================================================= //
-void exec_commands_on_new_session(char ***commands, size_t amount_commads)
-{
+void exec_commands_on_new_session(char ***commands, size_t amount_commads) {
   setsid();
 
-  for (int i = 0; i < amount_commads; i++)
-  {
+  for (size_t i = 0; i < amount_commads; i++) {
     int pid = fork();
 
-    if (pid == -1)
-    {
+    if (pid == -1) {
       perror("fork error: ");
       exit(-1);
     }
 
-    if (pid == 0)
-    {
+    if (pid == 0) {
       execvp(commands[i][0], commands[i]);
       perror("exec error: ");
       exit(-1);
@@ -44,11 +39,9 @@ void exec_commands_on_new_session(char ***commands, size_t amount_commads)
 }
 
 // ================== //
-char ***init_buffer()
-{
+char ***init_buffer() {
   char ***buffer = calloc(AMOUNT_COMMANDS, sizeof(char **));
-  for (int x = 0; x < AMOUNT_COMMANDS; x++)
-  {
+  for (int x = 0; x < AMOUNT_COMMANDS; x++) {
     buffer[x] = calloc(AMOUNT_ARGS, sizeof(char *));
   }
 
@@ -56,93 +49,101 @@ char ***init_buffer()
 }
 
 // ============================================ //
-char ***read_shell_input(char ***buffer, bool *foreground_exec, int * commands_amount)
-{
+char ***read_shell_input(char ***buffer, bool *foreground_exec,
+                         int *commands_amount) {
   bool error = false;
   size_t size = 0;
   ssize_t char_amount;
   char *line = NULL;
   // scanf("%[^\n]%*c", line);
-  
+
   char_amount = getline(&line, &size, stdin);
-  if(char_amount == 1) return NULL;
+  if (char_amount == 1)
+    return NULL;
 
   // Removing whitespaces from right side
   r_strip(line);
 
   // exiting
-  if(!strcmp(line, "exit")) exit(EXIT_SUCCESS);
+  if (!strcmp(line, "exit"))
+    exit(EXIT_SUCCESS);
 
   // Setting foreground flag
-  if (line[strlen(line) - 1] == '%')
-  {
+  if (line[strlen(line) - 1] == '%') {
     printf("foreground\n"); // debug
     *foreground_exec = true;
   }
 
   // Spliting commands
   char **commands = calloc(AMOUNT_COMMANDS, sizeof(char *));
-  if(!split_commands(line, commands_amount, commands)){
+  if (!split_commands(line, commands_amount, commands)) {
     end_commands(commands);
     return NULL;
-  } 
+  }
 
   // Spliting args
-  if(!split_args(commands, buffer)){
+  if (!split_args(commands, buffer)) {
     end_commands(commands);
     return NULL;
-  } 
-    
+  }
+
   // end_commands(commands);
   return buffer;
 }
 
 //=================================================================//
-char ** split_commands(char * line, int * counter, char ** commands){
+static char **split_commands(char *line, int *counter, char **commands) {
   bool error = false;
   int command_counter = 0;
   char *command = strtok(line, "<3");
-  while (command)
-  {
-    if(command_counter >= AMOUNT_COMMANDS){
-      printf(COLOR_RED"You can not type more than %d commands!\n"COLOR_RESET, AMOUNT_COMMANDS);
+  while (command) {
+    if (command_counter >= AMOUNT_COMMANDS) {
+      printf(COLOR_RED "You can not type more than %d commands!\n" COLOR_RESET,
+             AMOUNT_COMMANDS);
       error = true;
       break;
     }
     // printf("command = %s\n", command); // debug
     commands[command_counter] = strdup(command);
-    // printf("args[command_counter] = %s\n", commands[command_counter]); // debug
+    // printf("args[command_counter] = %s\n", commands[command_counter]); //
+    // debug
     command = strtok(NULL, "<3");
     command_counter++;
   }
 
   *counter = command_counter;
 
-  if(error) return NULL;
+  if (error)
+    return NULL;
   return commands;
 }
 
 //==============================================//
-int split_args(char ** commands, char *** buffer){
+static int split_args(char **commands, char ***buffer) {
   bool error = false;
   char *arg = NULL;
   int args_counter;
-  for(int x = 0; x < AMOUNT_COMMANDS; x++)
-  {
+  for (int x = 0; x < AMOUNT_COMMANDS; x++) {
     // break if command at this index do not exists
-    if(!commands[x]) break;
+    if (!commands[x])
+      break;
 
     // separating args
     arg = strtok(commands[x], " ");
-    args_counter=0;
-    while (arg)
-    {
-      if(args_counter >= AMOUNT_ARGS){
-        printf(COLOR_RED"You can not type more than %d args!\n"COLOR_RESET, AMOUNT_ARGS);
+    args_counter = 0;
+    while (arg) {
+      if (args_counter >= AMOUNT_ARGS) {
+        printf(COLOR_RED "You can not type more than %d args!\n" COLOR_RESET,
+               AMOUNT_ARGS);
         error = true;
         break;
       }
       // printf("arg = %s\n", arg);
+
+      if (!strcmp(arg, "%")) {
+        buffer[x][args_counter] = NULL;
+        break;
+      }
 
       buffer[x][args_counter] = strdup(arg);
 
@@ -151,43 +152,48 @@ int split_args(char ** commands, char *** buffer){
     }
   }
 
-  if(error) return 0;
-  return 1;   
+  if (error)
+    return 0;
+  return 1;
 }
 
 //=================================//
-void end_commands(char ** commands){
-  if(commands){
-    for(int x=0; x<AMOUNT_COMMANDS; x++){
-      if(commands[x]) free(commands[x]);
+void end_commands(char **commands) {
+  if (commands) {
+    for (int x = 0; x < AMOUNT_COMMANDS; x++) {
+      if (commands[x])
+        free(commands[x]);
     }
     free(commands);
   }
 }
 
 //============================//
-void set_buffer(char ***buffer){
-  if(!buffer) perror("Could not set unexisting buffer!\n");
-  for(int x=0; x<AMOUNT_COMMANDS; x++){
-    for(int y=0; y<AMOUNT_ARGS; y++){
+void set_buffer(char ***buffer) {
+  if (!buffer)
+    perror("Could not set unexisting buffer!\n");
+  for (int x = 0; x < AMOUNT_COMMANDS; x++) {
+    for (int y = 0; y < AMOUNT_ARGS; y++) {
       buffer[x][y] = NULL;
     }
   }
 }
 
 //==============================//
-void end_buffer(char *** buffer){
-  if(!buffer) perror("Could not set unexisting buffer!\n");
-  for(int x=0; x<AMOUNT_COMMANDS; x++){
-    for(int y=0; y<AMOUNT_ARGS; y++){
+void end_buffer(char ***buffer) {
+  if (!buffer)
+    perror("Could not set unexisting buffer!\n");
+  for (int x = 0; x < AMOUNT_COMMANDS; x++) {
+    for (int y = 0; y < AMOUNT_ARGS; y++) {
       free(buffer[x][y]);
     }
     free(buffer[x]);
   }
 }
 
-void r_strip(char * string){ 
-  while(string[strlen(string)-1] == ' ' || string[strlen(string)-1] == '\n'){
-    string[strlen(string)-1] = '\0';
+void r_strip(char *string) {
+  while (string[strlen(string) - 1] == ' ' ||
+         string[strlen(string) - 1] == '\n') {
+    string[strlen(string) - 1] = '\0';
   }
 }
--- src/main.c
+++ src/main.c
@@ -8,8 +8,7 @@
 #include "../include/helpers.h"
 
 // Client================================//
-int main(int argc, char **argv) {
-  // exec_commands_on_new_session(create_test_commands(), AMOUNT_COMMANDS);
+int main() {
   bool foreground_execution = false;
   int commands_amount=0;
   char *** buffer = init_buffer();
@@ -19,39 +18,26 @@ int main(int argc, char **argv) {
     printf(COLOR_GREEN_BOLD"acsh > "COLOR_RESET);
     read_shell_input(buffer, &foreground_execution, &commands_amount);
 
-    // How to cross the buffer
-    // for(int i=0; i<commands_amount; i++){
-      // printf("command %d\n", i+1);
-      // for(int j=0; j<AMOUNT_ARGS; j++){
-        // if(!buffer[i][j]) break;
-        // printf("arg %d: %s\n", j+1, buffer[i][j]);       
-      // }
-    // }
+    if (!foreground_execution) {
+      exec_commands_on_new_session(buffer, commands_amount);
+    } else {
+      // NOTE: if executing on foreground, we know there's just one command
+      pid_t pid = fork();
+
+      if (pid == -1) {
+        perror("fork error: ");
+        exit(-1);
+      }
+
+      if (pid == 0) {
+        execvp(buffer[0][0], buffer[0]);
+        perror("exec error: ");
+        exit(-1);
+      } else {
+        wait(NULL);
+      }
+    }
   }
 
   end_buffer(buffer);
-
-  return 0;  
-}
-
-// Testing commands=============//
-char ***create_test_commands() {
-  char ***commands = malloc(AMOUNT_COMMANDS * sizeof(char ***)); 
-
-  for (int i = 0; i < AMOUNT_COMMANDS; i++) {
-    commands[i] = calloc(AMOUNT_ARGS, sizeof(char **)); 
-
-    char test_command[100];
-    // sprintf(test_command, "command %d ", i);
-    commands[i][0] = strdup("sleep");
-    commands[i][1] = strdup("60s");
-
-    // for (int j = 1; j < AMOUNT_ARGS; j++) {
-    //   char test_arg[100];
-    //   sprintf(test_arg, "arg %d ", j - 1);
-    //   commands[i][j] = strdup(test_arg);
-    // }
-  }
-
-  return commands;
 }
