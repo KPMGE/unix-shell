e38eb57eb72aa10173f7a78e9efa09e24d4e25c1
jvmoraiscb <jv.moraiscb@gmail.com>
2023-06-27 20:16:01 -0300

    :heavy_check_mark: add signal handlers

--- src/helpers.c
+++ src/helpers.c
@@ -1,10 +1,10 @@
-#include <helpers.h>
 #include <errno.h>
+#include <helpers.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
 // Prototypes========//
 // Public==========//
@@ -23,107 +23,108 @@ static int split_args(char **commands, char ***buffer);
 
 //===========================================//
 void exec_command(char *command, char **args) {
-  execvp(command, args);
-  perror("exec error: ");
-  exit(EXIT_FAILURE);
+    execvp(command, args);
+    perror("exec error: ");
+    exit(EXIT_FAILURE);
 }
 
 // ======================================================================= //
 void exec_commands_on_new_session(char ***buffer, size_t amount_commads) {
-  if (setsid() == -1) {
-    perror("setsid error: ");
-    exit(EXIT_FAILURE);
-  }
-
-  for (size_t i = 1; i < amount_commads; i++) {
-    pid_t pid = fork();
-
-    if (pid == -1) {
-      perror("fork error: ");
-      exit(-1);
+    if (setsid() == -1) {
+        perror("setsid error: ");
+        exit(EXIT_FAILURE);
     }
 
-    if (pid == 0) {
-      exec_command(buffer[i][0], buffer[i]);
-    }
-  }
+    for (size_t i = 1; i < amount_commads; i++) {
+        pid_t pid = fork();
 
-  exec_command(buffer[0][0], buffer[0]);
+        if (pid == -1) {
+            perror("fork error: ");
+            exit(-1);
+        }
+
+        if (pid == 0) {
+            exec_command(buffer[i][0], buffer[i]);
+        }
+    }
+
+    exec_command(buffer[0][0], buffer[0]);
 }
 
 // ================== //
 char ***init_buffer() {
-  char ***buffer = calloc(AMOUNT_COMMANDS, sizeof(char **));
-  for (int x = 0; x < AMOUNT_COMMANDS; x++) {
-    buffer[x] = calloc(AMOUNT_ARGS, sizeof(char *));
-  }
+    char ***buffer = calloc(AMOUNT_COMMANDS, sizeof(char **));
+    for (int x = 0; x < AMOUNT_COMMANDS; x++) {
+        buffer[x] = calloc(AMOUNT_ARGS, sizeof(char *));
+    }
 
-  return buffer;
+    return buffer;
 }
 
 // ============================================ //
 char ***read_shell_input(char ***buffer, bool *foreground_exec, int *commands_amount) {
-  size_t size = 0;
-  ssize_t char_amount;
-  char *line = NULL;
+    size_t size = 0;
+    ssize_t char_amount;
+    char *line = NULL;
 
-  char_amount = getline(&line, &size, stdin);
+    char_amount = getline(&line, &size, stdin);
 
-  // Handling exit conditions and formatting
-  if(!validate_line(line, char_amount, foreground_exec, buffer))
-    return NULL;
+    // Handling exit conditions and formatting
+    if (!validate_line(line, char_amount, foreground_exec, buffer))
+        return NULL;
 
-  // Spliting commands
-  char **commands = calloc(AMOUNT_COMMANDS, sizeof(char *));
-  if (!split_commands2(line, commands_amount, commands)) {
+    // Spliting commands
+    char **commands = calloc(AMOUNT_COMMANDS, sizeof(char *));
+    if (!split_commands2(line, commands_amount, commands)) {
+        end_commands(commands);
+        return NULL;
+    }
+
+    // Spliting args
+    if (!split_args(commands, buffer)) {
+        end_commands(commands);
+        return NULL;
+    }
+
+    free(line);
     end_commands(commands);
-    return NULL;
-  }
 
-  // Spliting args
-  if (!split_args(commands, buffer)) {
-    end_commands(commands);
-    return NULL;
-  }
-
-  free(line);
-  end_commands(commands);
-
-  return buffer;
+    return buffer;
 }
 
 //================================================================//
 char **split_commands2(char *line, int *counter, char **commands) {
-  char token[250];
-  int k = 0, commands_count = 0;
-  memset(token, 0, 250 * sizeof(char));
+    char token[250];
+    int k = 0, commands_count = 0;
+    memset(token, 0, 250 * sizeof(char));
 
-  for (size_t i = 0; i < strlen(line) - 1; i++) {
-    if (line[i] == '<' && line[i + 1] == '3') {
-      token[k] = '\0';
-      commands[commands_count++] = strdup(token);
+    for (size_t i = 0; i < strlen(line) - 1; i++) {
+        if (line[i] == '<' && line[i + 1] == '3') {
+            token[k] = '\0';
+            commands[commands_count++] = strdup(token);
 
-      k = 0; i++;
-      memset(token, 0, 250 * sizeof(char));
+            k = 0;
+            i++;
+            memset(token, 0, 250 * sizeof(char));
 
-      continue;
+            continue;
+        }
+
+        token[k++] = line[i];
+        token[k] = line[i + 1];
     }
 
-    token[k++] = line[i];
-    token[k] = line[i + 1];
-  }
+    token[k + 1] = '\0';
+    commands[commands_count++] = strdup(token);
 
-  token[k + 1] = '\0';
-  commands[commands_count++] = strdup(token);
+    if (commands_count >= AMOUNT_COMMANDS) {
+        printf(COLOR_RED "You can not type more than %d commands!\n" COLOR_RESET, AMOUNT_COMMANDS);
+        return NULL;
+    }
 
-  if (commands_count >= AMOUNT_COMMANDS) {
-    printf(COLOR_RED "You can not type more than %d commands!\n" COLOR_RESET, AMOUNT_COMMANDS);
-    return NULL;
-  }
+    *counter = commands_count;
 
-  *counter = commands_count;
-
-  return commands;
+    return commands;
 }
 
 //=================================================================//
@@ -155,127 +156,127 @@ char **split_commands2(char *line, int *counter, char **commands) {
 
 //==============================================//
 static int split_args(char **commands, char ***buffer) {
-  bool error = false;
-  char *arg = NULL;
-  int args_counter;
-  for (int x = 0; x < AMOUNT_COMMANDS; x++) {
-    // break if command at this index do not exists
-    if (!commands[x])
-      break;
+    bool error = false;
+    char *arg = NULL;
+    int args_counter;
+    for (int x = 0; x < AMOUNT_COMMANDS; x++) {
+        // break if command at this index do not exists
+        if (!commands[x])
+            break;
 
-    // separating args
-    arg = strtok(commands[x], " ");
-    args_counter = 0;
-    while (arg) {
-      if (args_counter >= AMOUNT_ARGS) {
-        printf(COLOR_RED "You can not type more than %d args!\n" COLOR_RESET,
-               AMOUNT_ARGS);
-        error = true;
-        break;
-      }
+        // separating args
+        arg = strtok(commands[x], " ");
+        args_counter = 0;
+        while (arg) {
+            if (args_counter >= AMOUNT_ARGS) {
+                printf(COLOR_RED "You can not type more than %d args!\n" COLOR_RESET,
+                       AMOUNT_ARGS);
+                error = true;
+                break;
+            }
 
-      if (!strcmp(arg, "%")) {
-        buffer[x][args_counter] = NULL;
-        break;
-      }
+            if (!strcmp(arg, "%")) {
+                buffer[x][args_counter] = NULL;
+                break;
+            }
 
-      buffer[x][args_counter] = strdup(arg);
+            buffer[x][args_counter] = strdup(arg);
 
-      arg = strtok(NULL, " ");
-      args_counter++;
+            arg = strtok(NULL, " ");
+            args_counter++;
+        }
     }
-  }
 
-  if (error)
-    return 0;
-  return 1;
+    if (error)
+        return 0;
+    return 1;
 }
 
 //=================================//
 void end_commands(char **commands) {
-  if (commands) {
-    for (int x = 0; x < AMOUNT_COMMANDS; x++) {
-      if (commands[x]) {
-        free(commands[x]);
-      }
+    if (commands) {
+        for (int x = 0; x < AMOUNT_COMMANDS; x++) {
+            if (commands[x]) {
+                free(commands[x]);
+            }
+        }
+        free(commands);
     }
-    free(commands);
-  }
 }
 
 //============================//
 void set_buffer(char ***buffer) {
-  if (!buffer) {
-    perror("Could not set unexisting buffer!\n");
-  }
-
-  for (int x = 0; x < AMOUNT_COMMANDS; x++) {
-    for (int y = 0; y < AMOUNT_ARGS; y++) {
-      if (buffer[x][y]) {
-        free(buffer[x][y]);
-        buffer[x][y] = NULL;
-      }
+    if (!buffer) {
+        perror("Could not set unexisting buffer!\n");
     }
-  }
+
+    for (int x = 0; x < AMOUNT_COMMANDS; x++) {
+        for (int y = 0; y < AMOUNT_ARGS; y++) {
+            if (buffer[x][y]) {
+                free(buffer[x][y]);
+                buffer[x][y] = NULL;
+            }
+        }
+    }
 }
 
 //==============================//
 void end_buffer(char ***buffer) {
-  if (!buffer) {
-    perror("Could not set unexisting buffer!\n");
-  }
-
-  for (int x = 0; x < AMOUNT_COMMANDS; x++) {
-    for (int y = 0; y < AMOUNT_ARGS; y++) {
-      if (buffer[x][y]) {
-        free(buffer[x][y]);
-      }
+    if (!buffer) {
+        perror("Could not set unexisting buffer!\n");
     }
 
-    if (buffer[x]) {
-      free(buffer[x]);
-    }
-  }
+    for (int x = 0; x < AMOUNT_COMMANDS; x++) {
+        for (int y = 0; y < AMOUNT_ARGS; y++) {
+            if (buffer[x][y]) {
+                free(buffer[x][y]);
+            }
+        }
 
-  free(buffer);
+        if (buffer[x]) {
+            free(buffer[x]);
+        }
+    }
+
+    free(buffer);
 }
 
 //========================//
 void r_strip(char *string) {
-  while (string[strlen(string) - 1] == ' ' ||
-         string[strlen(string) - 1] == '\n') {
-    string[strlen(string) - 1] = '\0';
-  }
+    while (string[strlen(string) - 1] == ' ' ||
+           string[strlen(string) - 1] == '\n') {
+        string[strlen(string) - 1] = '\0';
+    }
 }
 
 //=======================================================================//
 static char *validate_line(char *line, int char_amount, bool *foreground_exec, char ***buffer) {
-  // If char is '\n'
-  if (char_amount == 1) {
-    free(line);
-    return NULL;
-  }
+    // If char is '\n'
+    if (char_amount == 1) {
+        free(line);
+        return NULL;
+    }
 
-  // Removing whitespaces from right side
-  r_strip(line);
+    // Removing whitespaces from right side
+    r_strip(line);
 
-  // exiting
-  if (!strcmp(line, "exit")) {
-    free(line);
-    end_buffer(buffer);
-    exit(EXIT_SUCCESS);
-  }
+    // exiting
+    if (!strcmp(line, "exit")) {
+        free(line);
+        end_buffer(buffer);
+        exit(EXIT_SUCCESS);
+    }
 
-  // Setting foreground flag
-  if (line[strlen(line) - 1] == '%') {
-    *foreground_exec = true;
-  } else {
-    *foreground_exec = false;
-  }
+    // Setting foreground flag
+    if (line[strlen(line) - 1] == '%') {
+        *foreground_exec = true;
+    } else {
+        *foreground_exec = false;
+    }
 
-  return line;
+    return line;
 }
 
 bool is_cd_function(char *str) {
-  return !strcmp(str, "cd");
+    return !strcmp(str, "cd");
 }
--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <helpers.h>
+#include <signal.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,53 +8,81 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+void signal_handler(int signum);
+
+bool foreground_execution = false;
+pid_t foreground_pid = 0;
+
 int main() {
-  bool foreground_execution = false;
-  int commands_amount = 0;
-  char ***buffer = init_buffer();
+    int commands_amount = 0;
+    char ***buffer = init_buffer();
 
-  while (true) {
-    printf(COLOR_GREEN_BOLD "acsh > " COLOR_RESET);
+    signal(SIGINT, signal_handler);
+    signal(SIGQUIT, signal_handler);
+    signal(SIGTSTP, signal_handler);
 
-    if (!read_shell_input(buffer, &foreground_execution, &commands_amount)) {
-      continue;
+    while (true) {
+        printf(COLOR_GREEN_BOLD "acsh > " COLOR_RESET);
+
+        if (!read_shell_input(buffer, &foreground_execution, &commands_amount)) {
+            continue;
+        }
+
+        if (is_cd_function(buffer[0][0])) {
+            if (chdir(buffer[0][1]) != 0) {
+                perror("chdir error: ");
+                exit(EXIT_FAILURE);
+            }
+
+            set_buffer(buffer);
+            continue;
+        }
+
+        pid_t pid = fork();
+
+        if (pid == -1) {
+            perror("fork error: ");
+            exit(EXIT_FAILURE);
+        }
+
+        if (pid == 0) {
+            if (foreground_execution) {
+                exec_command(buffer[0][0], buffer[0]);
+            }
+
+            if (!foreground_execution) {
+                exec_commands_on_new_session(buffer, commands_amount);
+            }
+        } else {
+            set_buffer(buffer);
+            if (foreground_execution) {
+                foreground_pid = pid;
+                wait(NULL);
+            } else {
+                waitpid(pid, NULL, WNOHANG);
+            }
+        }
     }
 
-    if (is_cd_function(buffer[0][0])) {
-      if (chdir(buffer[0][1]) != 0) {
-        perror("chdir error: ");
-        exit(EXIT_FAILURE);
-      }
+    end_buffer(buffer);
+}
 
-      set_buffer(buffer);
-      continue;
-    }
-
-    pid_t pid = fork();
-
-    if (pid == -1) {
-      perror("fork error: ");
-      exit(EXIT_FAILURE);
-    }
-
-    if (pid == 0) {
-      if (foreground_execution) {
-        exec_command(buffer[0][0], buffer[0]);
-      }
-
-      if (!foreground_execution) {
-        exec_commands_on_new_session(buffer, commands_amount);
-      }
+void signal_handler(int signum) {
+    if (foreground_execution) {
+        kill(foreground_pid, SIGKILL);
+        foreground_execution = false;
+        printf("\n");
     } else {
-      set_buffer(buffer);
+        char signalChar = '?';
+        if (signum == SIGINT)
+            signalChar = 'C';
+        if (signum == SIGQUIT)
+            signalChar = '\\';
+        if (signum == SIGTSTP)
+            signalChar = 'Z';
 
-      if (foreground_execution) {
-        wait(NULL);
-      } else {
-        waitpid(pid, NULL, WNOHANG);
-      }
+        printf(COLOR_RED "ERROR: You can not terminate the program via Ctrl-%c signal.\n" COLOR_RESET, signalChar);
+        printf(COLOR_GREEN_BOLD "acsh > " COLOR_RESET);
+        fflush(stdout);  // force buffer
     }
-  }
-
-  end_buffer(buffer);
 }
